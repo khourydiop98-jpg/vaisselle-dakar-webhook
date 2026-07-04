30068140e53a4651ec39fb50683755f04a3b6e76
khourydiop98-jpg <khourydiop98@gmail.com>
2026-07-04 16:36:52 +0100

    Enhance webhook processing for message types
    Updated webhook handling to differentiate between image and text messages, and added new webhook URLs.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = 'asna_whatsapp';
-const MAKE_WEBHOOK = 'https://hook.eu1.make.com/jqzvg3s7m7nyqpvfbh7agtv6a2rn25ex';
+const MAKE_WEBHOOK_1 = 'https://hook.eu1.make.com/jqzvg3s7m7nyqpvfbh7agtv6a2rn25ex';
+const MAKE_WEBHOOK_3 = 'https://hook.eu1.make.com/f8p6uo7mzv9hg3d37n0s399s6iyka7q7';
+const META_TOKEN = 'EAASSNHnLMcIBR6bI26BSJKOAxTjHsoc6wOZBGFpICz62q9i9dYp7NVnsVp54b2Sg9fAQgQhnAbjoNYJpSWMjGU2GpVqvto4ND8809TzwzTa1eCg4xDbdlbI50o2aZAw1ZC4Se7l3rSPPIM2gbAw5pWCUCZCOZCHEnD5N3m5cZCTnSpm7ZCgzMA2X5f3qes9eW2fZCZAf3s3YASOdCjszwLngiHVAJZBtNL8xKw9zwH';
+const PHONE_ID = '1186412447888256';
 
 app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
@@ -18,8 +21,23 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-  axios.post(MAKE_WEBHOOK, req.body);
+  const body = req.body;
+  
+  if (body.object === 'whatsapp_business_account') {
+    const messages = body.entry?.[0]?.changes?.[0]?.value?.messages;
+    
+    if (messages && messages.length > 0) {
+      const message = messages[0];
+      
+      if (message.type === 'image') {
+        axios.post(MAKE_WEBHOOK_1, body);
+      } else if (message.type === 'text') {
+        axios.post(MAKE_WEBHOOK_3, body);
+      }
+    }
+  }
+  
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log('Serveur démarré sur port 3000'));
+app.listen(3000, () => console.log('Serveur Asna Boutique démarré sur port 3000'));
